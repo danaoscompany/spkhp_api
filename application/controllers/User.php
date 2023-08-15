2ae69ead28d2db517b4282324125d376d8288a8f
danaoscompany@gmail.com <danaoscompany@gmail.com>
2023-08-15 14:27:17 +0900

    a

---
+++ application/controllers/User.php
@@ -0,0 +1,48 @@
+<?php
+
+class User extends CI_Controller {
+  
+  public function get_criterias() {
+    $criterias = $this->db->query("SELECT * FROM `criterias` ORDER BY `name`")->result_array();
+    echo json_encode($criterias);
+  }
+  
+  public function add_criteria() {
+    $name = $this->input->post('name');
+    $type = intval($this->input->post('type'));
+    $this->db->insert("criterias", array(
+      'name' => $name,
+      'type' => $type
+    ));
+  }
+  
+  public function update_criteria() {
+      $criteriaID = intval($this->input->post('criteria_id'));
+      $name = $this->input->post('name');
+      $this->db->query("UPDATE `criterias` SET `name`='".$name."' WHERE `id`=".$criteriaID);
+    }
+  
+  public function get_criteria_values() {
+    $criteriaID = intval($this->input->post('criteria_id'));
+    $values = $this->db->query("SELECT * FROM `criteria_values` WHERE `criteria_id`=".$criteriaID." ORDER BY `value`")->result_array();
+    echo json_encode($values);
+  }
+  
+  public function add_criteria_value() {
+      $criteriaID = intval($this->input->post('criteria_id'));
+      $value = $this->input->post('value');
+      $weight = intval($this->input->post('weight'));
+      $this->db->insert("criteria_values", array(
+        'criteria_id' => $criteriaID,
+        'value' => $value,
+        'weight' => $weight
+      ));
+    }
+    
+  public function update_criteria_value() {
+        $id = intval($this->input->post('id'));
+        $value = $this->input->post('value');
+        $weight = intval($this->input->post('weight'));
+        $this->db->query("UPDATE `criteria_values` SET `value`='".$value."', `weight`=".$weight." WHERE `id`=".$id);
+      }
+}
