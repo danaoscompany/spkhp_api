5e1c57b86f5a342d929ed8a21b53509563fdb93d
danaoscompany@gmail.com <danaoscompany@gmail.com>
2023-08-15 15:34:50 +0900

    a

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -19,6 +19,7 @@ class User extends CI_Controller {
   public function update_criteria() {
       $criteriaID = intval($this->input->post('criteria_id'));
       $name = $this->input->post('name');
+      $type = intval($this->input->post("type"));
       $this->db->query("UPDATE `criterias` SET `name`='".$name."' WHERE `id`=".$criteriaID);
     }
   
